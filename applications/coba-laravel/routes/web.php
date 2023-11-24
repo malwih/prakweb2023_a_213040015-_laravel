f15fd7431f4148a58d6dbb29de1a38e2643f0416
malwih <malwihidayattogatorop@gmail.com>
2023-11-24 08:38:44 +0700

    Commit Video 23 Laravel

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AdminCategoryController;
 use App\Models\User;
 
 /*
@@ -67,3 +68,5 @@ Route::get('/dashboard/posts/checkSlug',[DashboardPostController::class, 'checkS
 
 Route::resource('/dashboard/posts', DashboardPostController::class)
 ->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
